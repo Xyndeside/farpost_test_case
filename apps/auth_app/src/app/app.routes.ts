7f16fc67b8283a38894181d79ecf796a5c02f755
Daniil Koval <112869367+Xyndeside@users.noreply.github.com>
2025-03-09 17:20:26 +1000

    Update app.routes.ts

--- apps/auth_app/src/app/app.routes.ts
+++ apps/auth_app/src/app/app.routes.ts
@@ -2,7 +2,5 @@ import { Route } from '@angular/router';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 
 export const appRoutes: Route[] = [
-    //С главной страницы перенаправляем на страницу аутентификации
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: 'auth', component: AuthPageComponent}
+    { path: '', component: AuthPageComponent}
 ];
